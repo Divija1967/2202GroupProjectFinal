628d55b263acbdc6714f786fe467c289ad31e2d4
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2021-12-07 00:04:25 -0500

    commit monday night

--- Driver.js
+++ Driver.js
@@ -1,99 +1,13 @@
-// Write a script to
-// Read the contents of both files (must use Promises when reading the text file)
-
-const {readFile} = require("fs");
-
-const fileRead = (path) => {
-  readFile(path, (error, fileBuffer) => {
-    if (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
-
-    const fileContent = fileBuffer.toString();
-
-    console.log(fileContent);
-  });
-};
-
-fileRead("package.json");
-fileRead("README.md");
-fileRead("index.js");
-
-// teddy bear
-const fs = require('fs');
-const { Module } = require('module');
-function getData(fileName, type) {
-    let promise = new Promise((resolve, reject) => {
-        fs.readFile(fileName, type, (error, data)=>{
-            if (data){
-                resolve(data);
-            } 
-            else{
-                reject(error);
-            }
-        });
-    });
-    return promise;
-
-}
-        module.exports = file;
-
-
-        function wordCount (text, cat, wordsArr){
-            let wordTypeObj = Object.getPrototypeOf(words);
-            wordTypeObj.category = cat;
-            let counter =0;
-            for(let i =0; i<wordsArr.length;i++){
-                let textArr = text.toUpperCase().split(/\s_/g);
-                for ( e of textArr){
-                    if (e.replace(".","")==wordsArr[i].toUpperCase()){
-                        counter++;
-                    }
-                }
-            }
-            wordTypeObj.occurences = counter;
-            return wordTypeObj;
-        }
- //       return new Promise(function(resolve, reject) {
-   //         for (let i = 0; i < data.length; i++) {
-       //         arr.push(data[i]);
-     //       }
-
- //           resolve();
-   //     });
-    //});
-//}
-
-getData('./Optimism_and_your_health.txt', 'sample').then((data) => {
-    console.log(data);
-});
-
-// Count the number of times the words of each category were mentioned. 
-//E.g. when a word in the synonyms category is mentioned you increment the count for that category.
-
-
-// Write the results in a file called results.txt
-
-//reading file without promise
-const fs = require('fs')
-
-fs.readFile('Optimism_and_your_health.txt', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log(data)
-})
-
 //reading file with promise
-const fs = require('fs')
+const fs = require('fs');
+const { prototype } = require('module');
+
 
 function getDataPromise(fileName, dataType){
 
     let promise = new Promise( (resolve, reject) => {
 
-        fs.readFile("Optimism_and_your_health.txt", "utf8", (error, data) => {
+        fs.readFile("Optimism and your health.txt", "utf8", (error, data) => {
             if (data)
             resolve(data);
             else
@@ -104,6 +18,80 @@ function getDataPromise(fileName, dataType){
     return promise;
 }
 
-getDataPromise('./Optimism_and_your_health.txt', 'sample').then((data) => {
-    console.log(data);
+// getDataPromise('./Optimism and your health.txt', 'sample').then((data) => {
+//    // console.log(data);
+// });
+
+
+const words = require('./words.json'); // imports and parses 
+// Count the number of times the words of each category were mentioned. 
+let arr = [];
+function wordCount (text, cat, wordsArr){
+    let wordCat = Object.getPrototypeOf(words);
+    wordCat.category = cat;
+    let counter =0;
+    for(let i =0; i<wordsArr.length;i++){
+        let textArr = text.toUpperCase().split(/\s_/g);
+        for ( e of textArr){
+            if (e.replace(".","")==wordsArr[i].toUpperCase()){
+                counter++;
+            }
+        }
+    }
+    wordCat.occurences = counter;
+    return wordCat;
+}
+
+// function wordCount (text, cat, wordsArr){
+//     let wordTypeObj = Object.getPrototypeOf(words);
+//     wordTypeObj.category = cat;
+//     let counter =0;
+//     for(let i =0; i<wordsArr.length;i++){
+//         let textArr = text.toUpperCase().split(/\s_/g);
+//         for ( e of textArr){
+//             if (e.replace(".","")==wordsArr[i].toUpperCase()){
+//                 counter++;
+//             }
+//         }
+//     }
+//     wordTypeObj.occurences = counter;
+//     return wordTypeObj;
+// }
+
+function getWords(fileName, dataType){
+
+    let promise = new Promise( (resolve, reject) => {
+
+        fs.readFile("words.json", "utf8", (error, data) => {
+            if (data)
+            resolve(data);
+            else
+            reject(error);
+        }  );
+    });
+
+    return promise;
+}
+
+let opt = "Optimism and your health.txt";
+getWords('./words.json', 'sample').then((data) => {
+   //console.log(data);
+
+    const obj = JSON.parse(data);
+    
 });
+    let synArr = obj.Synonyms;
+    console.loh(synArr);
+    let relArr = obj.Related;
+    let nearArr = obj.Near_Antonyms;
+    let antArr = obj.Antonyms;
+
+
+
+console.log(wordCount(opt, "Synonyms", arr));
+console.log(arr);
+
+//E.g. when a word in the synonyms category is mentioned you increment the count for that category.
+
+
+// Write the results in a file called results.txt
