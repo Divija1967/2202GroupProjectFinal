bb9e5d9f285430baee47644de4779d557b287c66
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2021-12-07 14:26:48 -0500

    prints 19, 22, 3, 5

--- Driver2.js
+++ Driver2.js
@@ -5,28 +5,27 @@
 
 const {promises: {readFile}} = require("fs");
 
-const words = require('./words.json');
-let synArr = words['Synonyms'];
-//console.log(synArr);
-let relArr = words['Related'];
-let nearArr = words['Near Antonyms'];
-let antArr = words['Antonyms'];
 
+const words = require('./words.json'); // imports and parses 
+    let synArr = words['Synonyms'];
+    let relArr = words['Related'];
+    let nearArr = words['Near Antonyms'];
+    let antArr = words['Antonyms'];
 // Reads both files
 Promise.all([
   readFile("words.json"),
   readFile("Optimism and your health.txt"),
 ]).then(([words, opt]) => {
-  
-    
-  //console.log(words.toString()); // prints json file as variable
-  //console.log(opt.toString()); // prints txt file as variable
+
+ // console.log(words.toString()); // prints json file as variable
+ // console.log(opt.toString()); // prints txt file as variable
 
   /*
   * *** currently prints a word count of zero for every category ***
   */
 
   console.log("Synonyms word count: " + wordCount(opt.toString(), "Synonyms", synArr));
+
   console.log("Related word count: " + wordCount(opt.toString(), "Related", relArr));
   console.log("Near Antonyms word count: " + wordCount(opt.toString(), "Near Antonyms", nearArr));
   console.log("Antonyms word count: " + wordCount(opt.toString(), "Antonyms", antArr));
@@ -42,20 +41,22 @@ Promise.all([
 function wordCount (text, cat, wordsArr){
     let wordTypeObj = Object.getPrototypeOf(words);
     wordTypeObj.category = cat;
-    let counter =0;
-    for(let i =0; i<wordsArr.length;i++){
-       // console.log(wordsArr[i]);
-        let textArr = text.toUpperCase().split(/\s_/g);
+    let counter = 0;
+    for(let i = 0; i<wordsArr.length;i++){
+        let textArr = text.toUpperCase().split(" ");
         let e = textArr[i];
-        //counter++;
         for (e of textArr){
-            if (e.replace(".","")==wordsArr[i].toUpperCase()){
+            if (e==wordsArr[i].toUpperCase()){
+                console.log("if");
+               // console.log(e);
                 counter++;
             }
-            counter++;
+            e++;
+            //counter++;
+         
         }
+        
     }
-    //console.log(counter);
-    //wordTypeObj.occurences = counter;
+    wordTypeObj.occurences = counter;
     return counter;
 }
